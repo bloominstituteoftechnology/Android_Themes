2cbdd5957dd83551973f6581b04b38582dd71fee
Jacob Seppanen <jbseppanen@gmail.com>
2018-11-15 15:53:32 -0800

    Reset

--- app/src/main/java/com/example/jacob/android_readinglist/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.jacob.android_readinglist;
-
-public class Constants {
-    public static final String UPDATE_REQUEST_CODE = "update_request_code";
-    public static final int EDIT_REQUEST_CODE = 1;
-}
