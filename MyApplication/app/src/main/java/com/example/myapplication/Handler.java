b878124933aa2626e79c9b79505899a28c3a44e0
BL-CoderOvertime <brandonlively@live.com>
2019-03-27 16:16:22 -0400

    Initial Commit - Added Project Folder v2

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Handler.java
@@ -0,0 +1,58 @@
+package com.example.myapplication;
+
+public class Handler {
+    private static int MAP_WIDTH = 1000;
+    private static int MAP_HEIGHT = 1000;
+    private static int MAP_DISPLAY_WIDTH = 14;
+    private static int MAP_DISPLAY_HEIGHT = 20;
+    private static int MAP_DENSITY = 50; // higher is less dense
+    private static int PLAYER_POS_Y = 1;
+    private static int PLAYER_POS_X = 1;
+    private static Map map;
+    private static char[][] displayArea;
+
+
+    public static void createMap() {
+        MAP_DISPLAY_HEIGHT = MainActivity.DISPLAY_HEIGHT/70;
+        MAP_DISPLAY_WIDTH = MainActivity.DISPLAY_WIDTH/35;
+        PLAYER_POS_Y = MAP_DISPLAY_HEIGHT/2;
+        PLAYER_POS_X = MAP_DISPLAY_WIDTH/2;
+        map = new Map(MAP_DENSITY, MAP_HEIGHT, MAP_WIDTH);
+        map.randomize();
+
+    }
+
+    public static char[][] mapDisplayArea(int y, int x) {
+        displayArea = new char[MAP_DISPLAY_HEIGHT][MAP_DISPLAY_WIDTH];
+        int counterY = 0;
+        for (int i = y; i < y + MAP_DISPLAY_HEIGHT; i++) {
+            int counterX = 0;
+            for (int j = x; j < x + MAP_DISPLAY_WIDTH; j++) {
+                displayArea[counterY][counterX] = map.getCharYX(i, j);
+                counterX++;
+            }
+            counterY++;
+        }
+        displayArea[PLAYER_POS_X][PLAYER_POS_Y] = 'P';
+        return displayArea;
+    }
+
+
+    public static boolean checkCollision(int y, int x){
+        y += PLAYER_POS_Y;
+        x += PLAYER_POS_X;
+        if(map.getCharYX(y,x) == 'X' || map.getCharYX(y,x) == '#'){
+            return true;
+        }
+        else{return false;}
+    }
+
+    public int getMapWidth() {
+        return MAP_WIDTH;
+    }
+
+    public int getMapHeight() {
+        return MAP_HEIGHT;
+    }
+
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.myapplication;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mapView;
+    Map map;
+    static int yPos = 20;
+    static int xPos = 20;
+    static int DISPLAY_WIDTH;
+    static int DISPLAY_HEIGHT;
+    Button upButton;
+    Button downButton;
+    Button leftButton;
+    Button rightButton;
+    Display display;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = getWindowManager().getDefaultDisplay();
+        DISPLAY_HEIGHT = display.getHeight();
+        DISPLAY_WIDTH = display.getWidth();
+        Log.i("test2", String.valueOf(DISPLAY_WIDTH) + "--" + String.valueOf(DISPLAY_HEIGHT));
+
+        mapView = findViewById(R.id.map_view);
+        Handler.createMap();
+
+        upButton = findViewById(R.id.button_up);
+        downButton = findViewById(R.id.button_down);
+        leftButton = findViewById(R.id.button_left);
+        rightButton = findViewById(R.id.button_right);
+
+        updateMap();
+
+        upButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Handler.checkCollision(yPos-1, xPos) != true) {
+                    yPos--;
+                    mapView.setText("");
+                    updateMap();
+                }
+            }
+        });
+
+        downButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Handler.checkCollision(yPos+1, xPos) != true) {
+                    yPos++;
+                    mapView.setText("");
+                    updateMap();
+                }
+            }
+        });
+
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Handler.checkCollision(yPos, xPos -1) != true) {
+                    xPos--;
+                    mapView.setText("");
+                    updateMap();
+                }
+            }
+        });
+
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Handler.checkCollision(yPos, xPos +1) != true) {
+                    xPos++;
+                    mapView.setText("");
+                    updateMap();
+                }
+            }
+        });
+
+
+
+    }
+
+    public void updateMap(){
+        printMapToView(charArr2ToStringArr(Handler.mapDisplayArea(yPos, xPos)));
+    }
+
+    public String[] charArr2ToStringArr(char[][] displayedMap){
+        String[] totalMap = new String[displayedMap.length];
+        for(int i = 0; i < displayedMap.length; i++){
+            String lineMap = "";
+            for(int j = 0; j < displayedMap[i].length; j++){
+                lineMap += displayedMap[i][j];
+            }
+            totalMap[i] = lineMap;
+        }
+        return totalMap;
+    }
+
+    public void printMapToView(String[] map){
+        mapView.setTypeface(Typeface.MONOSPACE);
+        mapView.setTextScaleX(1.5f);
+
+        for(int i = -2; i < map[1].length(); i++){
+            mapView.append("#");
+        }
+        mapView.append("\n");
+        for(String maps : map) {
+            mapView.append("#");
+            mapView.append(maps);
+            mapView.append("#\n");
+        }
+        for(int i = -2; i < map[1].length(); i++){
+            mapView.append("#");
+        }
+
+    }
+
+
+}
